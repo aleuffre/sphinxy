dfcf5a56c15c7d3e34460be57f0067fdf3c14df1
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2023-05-28 15:12:58 +0200

    Add documentation

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Sphinxy docs"
+copyright = "2023, Alessandro Uffreduzzi"
+author = "Alessandro Uffreduzzi"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",  # pull documentation from docstrings
+    "sphinx.ext.napoleon",  # how to parse google/numpy style docstrings
+]
+
+autoclass_content = "both"  # get docs from docstrings and from init file
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,27 @@
+.. Sphinxy docs documentation master file, created by
+   sphinx-quickstart on Sun May 28 14:35:31 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Sphinxy docs's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   modules
+
+
+The Egyptian Sphinx
+-------------------
+
+.. image:: _static/sphinx.png
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
